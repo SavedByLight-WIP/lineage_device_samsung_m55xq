fd863f173f37da039b28a1e461c01d3974d53271
Jamie Falconer <mrfluffyoven@gmail.com>
2026-03-12 16:28:19 +0000

    qssi > m55xq

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,4 +100,4 @@ BOARD_AVB_VENDOR_BOOT_ROLLBACK_INDEX_LOCATION := 1
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Inherit the proprietary files
-include vendor/samsung/qssi/BoardConfigVendor.mk
+include vendor/samsung/m55xq/BoardConfigVendor.mk
