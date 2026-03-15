9a1a1c92cedb1046c12661873fdca0a00b935c8d
Jamie Falconer <mrfluffyoven@gmail.com>
2026-03-15 14:57:33 +0000

    add device compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,8 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    device/samsung/m55xq/compatibility_matrix.device.xml
 
 # Inherit the proprietary files
 include vendor/samsung/m55xq/BoardConfigVendor.mk
