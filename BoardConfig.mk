2b88f576090696997d0179699b7a3b6d99822954
Jamie Falconer <mrfluffyoven@gmail.com>
2026-03-22 13:35:07 +0000

    fix module loading

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,8 +200,13 @@ DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     device/samsung/m55xq/compatibility_matrix.device.xml
 
-BOARD_VENDOR_KERNEL_MODULES += $(wildcard $(LOCAL_PATH)/rootdir/modules/vendor_dlkm/*.ko)
+# Recovery Modules
+BOARD_RECOVERY_KERNEL_MODULES_LOAD := $(LOCAL_PATH)/rootdir/modules/recovery/modules.load.recovery
 BOARD_RECOVERY_KERNEL_MODULES += $(wildcard $(LOCAL_PATH)/rootdir/modules/recovery/*.ko)
+
+# Vendor Modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD    := $(LOCAL_PATH)/rootdir/modules/vendor_dlkm/modules.load
+BOARD_VENDOR_KERNEL_MODULES += $(wildcard $(LOCAL_PATH)/rootdir/modules/vendor_dlkm/*.ko)
 
 # Inherit the proprietary files
 include vendor/samsung/m55xq/BoardConfigVendor.mk
