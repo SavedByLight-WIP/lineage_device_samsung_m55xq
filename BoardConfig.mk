4bedeebb6eda7423c95a2fd1037e10b8197580c2
Jamie Falconer <mrfluffyoven@gmail.com>
2026-03-26 12:22:24 +0000

    remove system_dlkm from dynamic partitions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,7 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
 BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
 BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
-BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext vendor product odm system_dlkm vendor_dlkm
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext vendor product odm vendor_dlkm
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
 BOARD_USES_VENDOR_RAMDISK := true
 BOARD_BUILD_VENDOR_RAMDISK_IMAGE := true
