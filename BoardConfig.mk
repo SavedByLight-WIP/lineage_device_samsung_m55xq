7a24de00d338ab0aff436b3a9f171fe359a02c5f
Jamie Falconer <mrfluffyoven@gmail.com>
2026-03-26 09:02:42 +0000

    update modules.load directorys

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,14 +200,18 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 # Recovery Modules
 BOARD_RECOVERY_KERNEL_MODULES_LOAD := $(DEVICE_PATH)/rootdir/modules/recovery/modules.load.recovery
 BOARD_RECOVERY_KERNEL_MODULES += $(wildcard $(DEVICE_PATH)/rootdir/modules/recovery/*.ko)
+BOARD_RECOVERY_KERNEL_MODULES += $(DEVICE_PATH)/rootdir/modules/recovery/modules.load.recovery
+
 
 # Vendor Modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(DEVICE_PATH)/rootdir/modules/vendor_dlkm/modules.load
 BOARD_VENDOR_KERNEL_MODULES += $(wildcard $(DEVICE_PATH)/rootdir/modules/vendor_dlkm/*.ko)
+BOARD_VENDOR_KERNEL_MODULES += $(DEVICE_PATH)/rootdir/modules/vendor_dlkm/modules.load
 
 # Vendor_boot Modules
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(DEVICE_PATH)/rootdir/modules/vendor_boot/modules.load.recovery
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(wildcard $(DEVICE_PATH)/rootdir/modules/vendor_boot/*.ko)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(DEVICE_PATH)/rootdir/modules/vendor_boot/modules.load.recovery
 
 # Inherit the proprietary files
 include vendor/samsung/m55xq/BoardConfigVendor.mk
