d9b92d00d54090f9fc02861edef1773ca95fbbfe
Jamie Falconer <mrfluffyoven@gmail.com>
2026-03-27 08:32:47 +0000

    fix compatibility matrix path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -207,7 +207,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    device/samsung/m55xq/etc/compatibility_matrix.device.xml
+    device/samsung/m55xq/etc/vintf/compatibility_matrix.device.xml
 
 # Modules
 BOARD_RECOVERY_KERNEL_MODULES += $(wildcard $(DEVICE_PATH)/rootdir/modules/recovery/*.ko)
