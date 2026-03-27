287ee9430d90be9735b5f9e5de8abb39de56523e
Jamie Falconer <mrfluffyoven@gmail.com>
2026-03-27 10:37:56 +0000

    fix cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,6 @@ TARGET_SCREEN_WIDTH := 2340
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 bootconfig console=null loop.max_part=7
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_IMAGE_NAME := Image
@@ -56,6 +55,20 @@ TARGET_KERNEL_SOURCE := kernel/samsung/m55xq
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_VERSION := 5.10.226
+
+BOARD_KERNEL_CMDLINE :=  \
+	video=vfb:640x400,bpp=32,memsize=3072000 \
+	printk.devkmsg=on \
+	firmware_class.path=/vendor/firmware_mnt/image \
+	console=null \
+	bootconfig \
+	androidboot.hardware=qcom \
+	hardware=qcom \
+	androidboot.memcg=1 \
+	androidboot.usbcontroller=a600000.dwc3 \
+	androidboot.init_fatal_panic=true \
+	androidboot.selinux=permissive \
+	loop.max_part=7
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
