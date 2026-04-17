3df5b9aa92b8dc8e145621027b76f012f81dcd8d
SavedByLight <mrfluffyoven@gmail.com>
2026-04-17 15:16:06 +0100

    remove BT profile conf

--- device.mk
+++ device.mk
@@ -233,7 +233,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor 
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/bluetooth/bt_profile.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_profile.conf \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
     
